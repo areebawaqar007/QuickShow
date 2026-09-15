121025e11dcf90451c0d758b58f4cd49530768f0
Areeba Waqar <areebawaqar007@gmail.com>
2026-09-16 03:32:06 +0500

    add footer and navbar with condition

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import { Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SeatLayout from './pages/SeatLayout'
-import MyBookings from './pages/MyBookings'
-import Favorite from './pages/Favorite'
-import MovieDetails from './pages/MovieDetails'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SeatLayout from "./pages/SeatLayout";
+import MyBookings from "./pages/MyBookings";
+import Favorite from "./pages/Favorite";
+import MovieDetails from "./pages/MovieDetails";
+import { Toaster } from "react-hot-toast";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
   return (
     <>
-      <Navbar />
-      <Routes>  
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<div>Movies Page</div>} />
         <Route path="/movies/:id" element={<MovieDetails />} />
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
-        <Route path="/favorite" element={ Favorite} />
-
-
+        <Route path="/favorite" element={Favorite} />
       </Routes>
-      <Footer />
-    </>
-  )
-}
 
-export default App
+      {!isAdminRoute && <Footer />}
+
+    </>
+  );
+};
+
+export default App;
